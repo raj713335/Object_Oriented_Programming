39103dd9a6c19a66644f34c214f3be738785c18d
ARNAB DAS <raj713335@gmail.com>
2022-02-26 13:13:17 +0530

    3

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 class Item:
-    def calculate_total_price(self, x, y):
-        return x*y
+    def __init__(self, name: str, price: float, quantity: int = 0):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def calculate_total_price(self):
+        return self.price * self.quantity
 
 
-item1 = Item()
-item1.name = "Phone"
-item1.price = 100
-item1.quantity = 5
-print(item1.calculate_total_price(item1.price, item1.quantity))
+item1 = Item("Phone", 100, 3)
 
-item2 = Item()
-item2.name = "Laptop"
-item2.price = 1000
-item2.quantity = 3
-print(item1.calculate_total_price(item2.price, item2.quantity))
+print(item1.calculate_total_price())
+
+item2 = Item("Laptop", 1000, 3)
+
+print(item2.calculate_total_price())
