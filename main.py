baf39e715f82352ccc9ff56da29cc5dbba6ed9f2
ARNAB DAS <raj713335@gmail.com>
2022-02-26 13:41:04 +0530

    4 def __repr__(self)

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 class Item:
+    # The pay rate after 20% discount
+    pay_rate = 0.8
+    all = []
+
     def __init__(self, name: str, price: float, quantity: int = 0):
+        # Run validations to the received arguments
+        assert price >= 0, f"Price {price} is not greater than zero!"
+        assert quantity >= 0, f"Price {quantity} is not greater than or equal to zero!"
+
+        # Assign to Self object
         self.name = name
         self.price = price
         self.quantity = quantity
 
+        # Actions to execute
+        Item.all.append(self)
+
     def calculate_total_price(self):
         return self.price * self.quantity
 
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
 
-item1 = Item("Phone", 100, 3)
+    def __repr__(self):
+        return f"Item('{self.name}',{self.price}, {self.quantity})"
 
-print(item1.calculate_total_price())
 
+item1 = Item("Phone", 100, 1)
 item2 = Item("Laptop", 1000, 3)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)
 
-print(item2.calculate_total_price())
+print(Item.all)
+
+for instance in Item.all:
+    print(instance.name)
