9d49f6ea402c9aaead75d5ecf9b97a054738f069
ARNAB DAS <raj713335@gmail.com>
2022-02-26 23:59:14 +0530

    6

--- main.py
+++ main.py
@@ -1,59 +1,9 @@
-import csv
+from phone import Phone
+
+phone1 = Phone("jscPhonev10", 500, 5,1)
+print(phone1.calculate_total_price())
+phone2 = Phone("jscPhonev20", 500, 5,1)
 
 
-class Item:
-    # The pay rate after 20% discount
-    pay_rate = 0.8
-    all = []
-
-    def __init__(self, name: str, price: float, quantity: int = 0):
-        # Run validations to the received arguments
-        assert price >= 0, f"Price {price} is not greater than zero!"
-        assert quantity >= 0, f"Price {quantity} is not greater than or equal to zero!"
-
-        # Assign to Self object
-        self.name = name
-        self.price = price
-        self.quantity = quantity
-
-        # Actions to execute
-        Item.all.append(self)
-
-    def calculate_total_price(self):
-        return self.price * self.quantity
-
-    def apply_discount(self):
-        self.price = self.price * self.pay_rate
-
-    @classmethod
-    def instantiate_from_csv(cls):
-        with open('items.csv', 'r') as f:
-            reader = csv.DictReader(f)
-            items = list(reader)
-
-        for item in items:
-            Item(
-                name=item.get('name'),
-                price=float(item.get('price')),
-                quantity=int(item.get('quantity')),
-            )
-
-    @staticmethod
-    def is_integer(num):
-        # We will count out the floats that are point zero
-        # For i.e: 5.0, 10.0
-        if isinstance(num, float):
-            # Count out the floats that are point zero
-            return num.is_integer()
-        elif isinstance(num, int):
-            return True
-        else:
-            return False
-
-    def __repr__(self):
-        return f"Item('{self.name}',{self.price}, {self.quantity})"
-
-
-print(Item.is_integer(7.0))
-
+print(Phone.all)
 
